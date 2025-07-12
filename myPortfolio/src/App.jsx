0467ce067a520b0ed04a2574d0c153dc5282af64
Suhail-Ahmed7 <suhailbhand7@gmail.com>
2025-07-12 20:50:25 +0500

    Fix case-sensitive import paths for Vercel

--- myPortfolio/src/App.jsx
+++ myPortfolio/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useRef } from 'react';
-import Navbar from './Components/Navbar';
+import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
 import Projects from './components/Projects';
